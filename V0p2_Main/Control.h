5a50641a218702512f2c4b9d94d8e21468b8d845
DamonHD <d@hd.org>
2015-09-24 14:33:34 +0100

    TODO-552: note BS EN 215:2004 limits
    Adjusted lower/FROST limit for ECO down 1C,
    so still above 5C minimum for BS EN 215:2004 S5.3.5
    and 5C hypothermia risk temperature.

--- V0p2_Main/Control.h
+++ V0p2_Main/Control.h
@@ -46,7 +46,9 @@ void loopOpenTRV();
 // Default frost (minimum) temperature in degrees C, strictly positive, in range [MIN_TARGET_C,MAX_TARGET_C].
 // Setting frost temperatures at a level likely to protect (eg) fridge/freezers as well as water pipes.
 // Note that 5C or below carries a risk of hypothermia: http://ipc.brookes.ac.uk/publications/pdf/Identifying_the_health_gain_from_retirement_housing.pdf
-#define BIASECO_FROST (max(7,MIN_TARGET_C)) // Target FROST temperature for ECO bias; must be in range [MIN_TARGET_C,BIASCOM_FROST[.
+// Other parts of the room may be somewhat colder than where the sensor is, so aim a little over 5C.
+// Note: BS EN 215:2004 S5.3.5 says maximum setting must be <= 32C, minimum in range [5C,12C].
+#define BIASECO_FROST (max(6,MIN_TARGET_C)) // Target FROST temperature for ECO bias; must be in range [MIN_TARGET_C,BIASCOM_FROST[.
 #define BIASCOM_FROST (max(12,MIN_TARGET_C)) // Target FROST temperature for Comfort bias; must be in range ]BIASECO_FROST,MAX_TARGET_C].
 #define FROST BIASECO_FROST
 // 18C is a safe room temperature even for the slightly infirm according to NHS England 2014:
