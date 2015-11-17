96b1b509f8f474be6dc4f57be5670f2f29b64d60
DamonHD <d@hd.org>
2015-11-17 11:20:50 +0000

    D21RM: WIP

--- V0p2_Main/AltMain.cpp
+++ V0p2_Main/AltMain.cpp
@@ -115,7 +115,7 @@ void POSTalt()
   // Flash locations
 	const char myPin[] PROGMEM = "0000";
 	const char myAPN[] PROGMEM = "m2mkit.telefonica.com"; // FIXME check this
-	const char myUDPAddr[] PROGMEM = "46.101.52.242";
+	const char myUDPAddr[] PROGMEM = "79.135.97.66"; // "46.101.52.242";
 	const char myUDPPort[] PROGMEM = "9999";
 
     static const void *SIM900_PIN      = (void *)myPin; // TODO confirm this address
