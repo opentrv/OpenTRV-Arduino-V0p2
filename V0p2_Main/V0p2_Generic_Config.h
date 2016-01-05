381c5ac31cb717e8912eef3033f95c9cbc84af3e
DamonHD <d@hd.org>
2016-01-05 15:35:05 +0000

    merge tidyup

--- V0p2_Main/V0p2_Generic_Config.h
+++ V0p2_Main/V0p2_Generic_Config.h
@@ -74,7 +74,7 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 //#define CONFIG_DE_TESTLAB // Deniz's test environment.
 //#define CONFIG_REV10_STRIPBOARD // REV10-based stripboard precursor for bus shelters
 //#define CONFIG_REV10 // Generic REV10 config
-#define CONFIG_REV10_BHR // REV10: boiler hub and stats relay.
+//#define CONFIG_REV10_BHR // REV10: boiler hub and stats relay.
 // TODO //#define CONFIG_REV10_SECURE_BOILERHUB_GSM_SECURE // REV10 PCB boiler hub, relay to GSM, 2015/12 secure protocol.
 //#define CONFIG_REV11_RFM23BTEST // Basic test to see if stats send
 //#define CONFIG_BAREBONES // No peripherals / on breadboard.
