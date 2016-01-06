1676a1cdd48898146dfbf511792aef0529225d6c
denzo <denzo@denzo-XPS-13-9343>
2016-01-06 17:31:18 +0000

    started updating REV10 busshelter config

--- V0p2_Main/V0p2_Generic_Config.h
+++ V0p2_Main/V0p2_Generic_Config.h
@@ -47,7 +47,7 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 //#define CONFIG_Trial2013Winter_Round2_BOILERHUB // REV2 cut4 as plain boiler hub.
 //#define CONFIG_Trial2013Winter_Round2_STATSHUB // REV2 cut4 as stats hub.
 //#define CONFIG_Trial2013Winter_Round2_NOHUB // REV2 cut4 as TX-only leaf node.
-#define CONFIG_DORM1 // REV7 / DORM1 Winter 2014/2015 all-in-one valve unit.
+//#define CONFIG_DORM1 // REV7 / DORM1 Winter 2014/2015 all-in-one valve unit.
 //#define CONFIG_DORM1_BOILER // REV8 / DORM1 Winter 2014/2015 boiler-control unit.
 //#define CONFIG_REV11_RAW_JSON // REV11 as raw JSON-only stats/sensor leaf.
 
@@ -72,7 +72,7 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 //#define CONFIG_REV9_STATS // REV9 as stats node, cut 2 of the board.
 //#define CONFIG_REV9_cut1 // REV9 as CC1 relay, cut1 of board.
 //#define CONFIG_DE_TESTLAB // Deniz's test environment.
-//#define CONFIG_REV10_STRIPBOARD // REV10-based stripboard precursor for bus shelters
+#define CONFIG_REV10_STRIPBOARD // REV10-based stripboard precursor for bus shelters
 //#define CONFIG_REV10 // Generic REV10 config
 //#define CONFIG_REV10_BHR // REV10: boiler hub and stats relay.
 // TODO //#define CONFIG_REV10_SECURE_BOILERHUB_GSM_SECURE // REV10 PCB boiler hub, relay to GSM, 2015/12 secure protocol.
@@ -889,75 +889,13 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 
 // REV8 + GSM Arduino shield + I2CEXT, see TODO-551
 
-#ifdef CONFIG_REV10_STRIPBOARD // REV10-based stripboard precursor for bus shelters
-// use alternative loop
-#define ALT_MAIN_LOOP
-#define V0p2_REV 10
-#define COMMON_SETTINGS
-// Defaults for V0.2; have to be undefined if not required.  ***
-// May require limiting clock speed and using some alternative peripherals/sensors.
-//#define SUPPLY_VOLTAGE_LOW_2AA
-// Provide software RTC support by default.
-//#define USE_RTC_INTERNAL_SIMPLE
-// IF DEFINED: this unit will act as a thermostat controlling a local TRV (and calling for heat from the boiler), else is a sensor/hub unit.
-#undef LOCAL_TRV
-// IF DEFINED: this unit *can* act as boiler-control hub listening to remote thermostats, possibly in addition to controlling a local TRV.  ***
-#undef ENABLE_BOILER_HUB
-// IF DEFINED: allow RX of stats frames.
-#undef ALLOW_STATS_RX
-// IF DEFINED: allow TX of stats frames.
-#define ALLOW_STATS_TX
-// IF DEFINED: allow minimal binary format in addition to more generic one: ~400 bytes code cost.
-#undef ENABLE_MINIMAL_STATS_TXRX
-// IF DEFINED: allow JSON stats frames alongside binary ones.
-//#undef ALLOW_JSON_OUTPUT
-// IF DEFINED: (default) forced always-on radio listen/RX, eg not requiring setup to explicitly enable. ***
-#undef ENABLE_DEFAULT_ALWAYS_RX
-// IF DEFINED: this unit supports CLI over the USB/serial connection, eg for run-time reconfig.
-#define SUPPORT_CLI
-// IF DEFINED: enable a full OpenTRV CLI.
-#define ENABLE_FULL_OT_CLI
-// IF DEFINED: enable a full OpenTRV UI with normal LEDs etc. ***
-//#define ENABLE_FULL_OT_UI
-// IF DEFINED: enable and extended CLI with a longer input buffer for example.
-#undef ENABLE_EXTENDED_CLI
-// IF DEFINED: minimise boot effort and energy eg for intermittently-powered energy-harvesting applications.  ***
-#undef MIN_ENERGY_BOOT
-// IF DEFINED: enable use of on-board SHT21 RH and temp sensor (in lieu of TMP112).   ***
-#undef ENABLE_SENSOR_SHT21
-// IF DEFINED: enable use AVR's 'idle' mode to stop the CPU but leave I/O (eg Serial) running to save power.
-// DHD20150920: CURRENTLY NOT RECOMMENDED AS STILL SEEMS TO CAUSE SOME BOARDS TO CRASH.
-#define ENABLE_USE_OF_AVR_IDLE_MODE
-#undef ENABLE_RADIO_RFM23B
-#undef RADIO_PRIMARY_RFM23B
-#define ENABLE_RADIO_SIM900   // Enable SIM900
-#define RADIO_PRIMARY_SIM900  // Assign SIM900
-// Define voice module
-#define ENABLE_VOICE_SENSOR
-#define ENABLE_OCCUPANCY_DETECTION_FROM_VOICE
-#define ENABLE_VOICE_STATS
-// Enable use of OneWire devices.
-#define SUPPORT_ONEWIRE
-// Enable use of DS18B20 temp sensor.
-#define SENSOR_DS18B20_ENABLE
-
-// things that break
-// IF DEFINED: basic FROST/WARM temperatures are settable.
-//#undef SETTABLE_TARGET_TEMPERATURES
-// IF DEFINED: use active-low LEARN button(s).  Needs SUPPORT_SINGLETON_SCHEDULE.  ***
-//#undef LEARN_BUTTON_AVAILABLE // OPTIONAL ON V0.09 PCB1  UI_Minimal.cpp:1180:32: error: 'handleLEARN' was not declared in this scope
-//#define USE_MODULE_FHT8VSIMPLE //Control.cpp:1322:27: error: 'localFHT8VTRVEnabled' was not declared in this scope
-
-#endif // CONFIG_REV10_BUSSHELTER
-
 #ifdef CONFIG_REV10 // REV10 base config
-// use alternative loop
 #define V0p2_REV 10
 #define COMMON_SETTINGS
 // IF DEFINED: this unit will act as a thermostat controlling a local TRV (and calling for heat from the boiler), else is a sensor/hub unit.
 #undef LOCAL_TRV
 // IF DEFINED: this unit *can* act as boiler-control hub listening to remote thermostats, possibly in addition to controlling a local TRV.  ***
-#define ENABLE_BOILER_HUB // TODO this should be defined but won't fit.
+#define ENABLE_BOILER_HUB
 // IF DEFINED: allow RX of stats frames.
 #define ALLOW_STATS_RX
 // IF DEFINED: allow TX of stats frames.
@@ -979,10 +917,6 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 // IF DEFINED: enable use AVR's 'idle' mode to stop the CPU but leave I/O (eg Serial) running to save power.
 // DHD20150920: CURRENTLY NOT RECOMMENDED AS STILL SEEMS TO CAUSE SOME BOARDS TO CRASH.
 #define ENABLE_USE_OF_AVR_IDLE_MODE
-#undef ENABLE_FULL_OT_UI
-
-//#undef ENABLE_RADIO_RFM23B
-//#undef RADIO_PRIMARY_RFM23B
 
 // Secondary radio
 // IF DEFINED: enable a secondary (typically WAN-relay) radio module.
@@ -991,6 +925,68 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2016
 #define RADIO_SECONDARY_SIM900  // Assign SIM900
 
 #endif // CONFIG_REV10
+
+
+#ifdef CONFIG_REV10_STRIPBOARD // REV10-based stripboard precursor for bus shelters
+
+#define V0p2_REV 10
+#define COMMON_SETTINGS
+// IF DEFINED: this unit will act as a thermostat controlling a local TRV (and calling for heat from the boiler), else is a sensor/hub unit.
+#undef LOCAL_TRV
+// IF DEFINED: this unit *can* act as boiler-control hub listening to remote thermostats, possibly in addition to controlling a local TRV.  ***
+#undef ENABLE_BOILER_HUB
+// IF DEFINED: allow RX of stats frames.
+#undef ALLOW_STATS_RX
+// IF DEFINED: (default) forced always-on radio listen/RX, eg not requiring setup to explicitly enable. ***
+#undef ENABLE_DEFAULT_ALWAYS_RX
+// IF DEFINED: allow TX of stats frames.
+#define ALLOW_STATS_TX
+// IF DEFINED: allow minimal binary format in addition to more generic one: ~400 bytes code cost.
+#undef ENABLE_MINIMAL_STATS_TXRX
+// IF DEFINED: this unit supports CLI over the USB/serial connection, eg for run-time reconfig.
+#define SUPPORT_CLI
+// IF DEFINED: enable a full OpenTRV CLI.
+#define ENABLE_FULL_OT_CLI
+// IF DEFINED: enable a full OpenTRV UI with normal LEDs etc. ***
+#undef ENABLE_FULL_OT_UI
+// IF DEFINED: enable and extended CLI with a longer input buffer for example.
+#undef ENABLE_EXTENDED_CLI
+// IF DEFINED: minimise boot effort and energy eg for intermittently-powered energy-harvesting applications.  ***
+#undef MIN_ENERGY_BOOT
+// IF DEFINED: enable use of on-board SHT21 RH and temp sensor (in lieu of TMP112).   ***
+#undef ENABLE_SENSOR_SHT21
+// IF DEFINED: enable use AVR's 'idle' mode to stop the CPU but leave I/O (eg Serial) running to save power.
+// DHD20150920: CURRENTLY NOT RECOMMENDED AS STILL SEEMS TO CAUSE SOME BOARDS TO CRASH.
+#define ENABLE_USE_OF_AVR_IDLE_MODE
+
+// Secondary radio
+#undef ENABLE_RADIO_RFM23B
+#undef RADIO_PRIMARY_RFM23B
+
+// IF DEFINED: enable a secondary (typically WAN-relay) radio module.
+#define ENABLE_RADIO_SECONDARY_MODULE
+#define ENABLE_RADIO_SIM900   // Enable SIM900
+#define RADIO_PRIMARY_SIM900  // Assign SIM900
+
+// Enable use of OneWire devices.
+#define SUPPORT_ONEWIRE
+// Enable use of DS18B20 temp sensor.
+#define SENSOR_DS18B20_ENABLE
+
+// Define voice module
+#define ENABLE_VOICE_SENSOR
+#define ENABLE_OCCUPANCY_DETECTION_FROM_VOICE
+#define ENABLE_VOICE_STATS
+
+// things that break
+// IF DEFINED: basic FROST/WARM temperatures are settable.
+//#undef SETTABLE_TARGET_TEMPERATURES
+// IF DEFINED: use active-low LEARN button(s).  Needs SUPPORT_SINGLETON_SCHEDULE.  ***
+//#undef LEARN_BUTTON_AVAILABLE // OPTIONAL ON V0.09 PCB1  UI_Minimal.cpp:1180:32: error: 'handleLEARN' was not declared in this scope
+//#define USE_MODULE_FHT8VSIMPLE //Control.cpp:1322:27: error: 'localFHT8VTRVEnabled' was not declared in this scope
+
+#endif // CONFIG_REV10_BUSSHELTER
+
 
 
 
