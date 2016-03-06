a2a8dbb499e2de0f4c29652a7a2805bee08dc07c
DamonHD <d@hd.org>
2016-03-06 23:51:55 +0000

    TODO-728: switched to decodeSecureSmallFrameSafely()

--- V0p2_Main/Messaging.cpp
+++ V0p2_Main/Messaging.cpp
@@ -345,20 +345,18 @@ if(!isOK) { DEBUG_SERIAL_PRINTLN_FLASHSTRING("!RX bad secure header"); }
   uint8_t senderNodeID[OTV0P2BASE::OpenTRV_Node_ID_Bytes];
   if(secureFrame && isOK)
     {
-    // Look up the full node ID of the sender in the associations table,
-    // and if successful then attempt to decode the message.
-    const int8_t index = OTV0P2BASE::getNextMatchingNodeID(0, sfh.id, sfh.getIl(), senderNodeID);
-#if 0 && defined(DEBUG)
-    if(index < 0) { DEBUG_SERIAL_PRINTLN_FLASHSTRING("!RX assoc"); }
-#endif
-    isOK = (index >= 0) &&
-           (0 != OTRadioLink::SimpleSecureFrame32or0BodyRXV0p2::getInstance()._decodeSecureSmallFrameFromID(&sfh, msg-1, msglen+1,
-                              OTAESGCM::fixed32BTextSize12BNonce16BTagSimpleDec_DEFAULT_STATELESS,
-                              senderNodeID, sizeof(senderNodeID),
-                              NULL, key,
-                              secBodyBuf, sizeof(secBodyBuf), decryptedBodyOutSize));
-#if 1 && defined(DEBUG)
-    if(!isOK) { DEBUG_SERIAL_PRINTLN_FLASHSTRING("!RX assoc/auth"); } // Missing association or filed auth.
+    // Look up full ID in associations table,
+    // validate RX message counter,
+    // authenticate and decrypt,
+    // update RX message counter.
+    isOK = (0 != OTRadioLink::SimpleSecureFrame32or0BodyRXV0p2::getInstance().decodeSecureSmallFrameSafely(&sfh, msg-1, msglen+1,
+                                            OTAESGCM::fixed32BTextSize12BNonce16BTagSimpleDec_DEFAULT_STATELESS,
+                                            NULL, key,
+                                            secBodyBuf, sizeof(secBodyBuf), decryptedBodyOutSize,
+                                            senderNodeID,
+                                            true));
+#if 1 // && defined(DEBUG)
+    if(!isOK) { OTV0P2BASE::serialPrintlnAndFlush(F("!RX assoc/auth")); } // Missing association or failed auth.
 #endif
     }
 
