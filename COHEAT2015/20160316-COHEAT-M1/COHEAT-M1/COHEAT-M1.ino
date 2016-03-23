f1ff501a8ac6ed52c29b0444575e2885af377c3a
DamonHD <d@hd.org>
2016-03-23 16:22:11 +0000

    COH-77: WIP

--- COHEAT2015/20160316-COHEAT-M1/COHEAT-M1/COHEAT-M1.ino
+++ COHEAT2015/20160316-COHEAT-M1/COHEAT-M1/COHEAT-M1.ino
@@ -1,6 +1,6 @@
 // Uncomment exactly one of the following CONFIG_... lines to select which board is being built for.
-//#define CONFIG_Trial2013Winter_Round2_CC1HUB // REV2 cut4 as CC1 hub.
-#define CONFIG_REV9 // REV9 as CC1 relay, cut 2 of the board.
+#define CONFIG_Trial2013Winter_Round2_CC1HUB // REV2 cut4 as CC1 hub.
+//#define CONFIG_REV9 // REV9 as CC1 relay, cut 2 of the board.
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -857,13 +857,12 @@ ISR(PCINT2_vect)
   const uint8_t changes = pins ^ prevStatePD;
   prevStatePD = pins;
 
-// FIXME FIXME FIXME
-//  // If an interrupt arrived from no other masked source then wake the CLI.
-//  // The will ensure that the CLI is active, eg from RX activity,
-//  // eg it is possible to wake the CLI subsystem with an extra CR or LF.
-//  // It is OK to trigger this from other things such as button presses.
-//  // FIXME: ensure that resetCLIActiveTimer() is inlineable to minimise ISR prologue/epilogue time and space.
-//  if(!(changes & MASK_PD & ~1)) { resetCLIActiveTimer(); }
+  // If an interrupt arrived from no other masked source then wake the CLI.
+  // The will ensure that the CLI is active, eg from RX activity,
+  // eg it is possible to wake the CLI subsystem with an extra CR or LF.
+  // It is OK to trigger this from other things such as button presses.
+  // FIXME: ensure that resetCLIActiveTimer() is inlineable to minimise ISR prologue/epilogue time and space.
+  if(!(changes & MASK_PD & ~1)) { resetCLIActiveTimer(); }
   }
 #endif
 
