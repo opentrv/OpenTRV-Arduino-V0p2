e384e551a659c3b71f3b1bd4ebe48f014bceedb2
DamonHD <d@hd.org>
2016-11-04 15:06:03 +0000

    typo

--- Arduino/V0p2_Main/V0p2_Main.h
+++ Arduino/V0p2_Main/V0p2_Main.h
@@ -467,7 +467,7 @@ extern OTV0P2BASE::VoiceDetectionQM1 Voice;
   static constexpr uint8_t m1 = MOTOR_DRIVE_MR;
   static constexpr uint8_t m2 = MOTOR_DRIVE_ML;
 #endif // HAS_DORM1_MOTOR_REVERSED
-typedef OTRadValve::ValveMotorDirectV1<m1, MOTOR_DRIVE_MR, m2, MOTOR_DRIVE_MC_AIN, decltype(Supply_cV), &Supply_cV> ValveDirect_t;
+typedef OTRadValve::ValveMotorDirectV1<m1, m2, MOTOR_DRIVE_MI_AIN, MOTOR_DRIVE_MC_AIN, decltype(Supply_cV), &Supply_cV> ValveDirect_t;
 extern ValveDirect_t ValveDirect;
 #endif
 
