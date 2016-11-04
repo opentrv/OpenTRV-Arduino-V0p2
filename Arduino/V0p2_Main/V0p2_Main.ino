b6aa6de2b1f9372f05813ce09780c6ded4f751bf
DamonHD <d@hd.org>
2016-11-04 21:58:45 +0000

    TODO-970: removing complexity from app
    4449 loc

--- Arduino/V0p2_Main/V0p2_Main.ino
+++ Arduino/V0p2_Main/V0p2_Main.ino
@@ -468,40 +468,7 @@ OTV0P2BASE::SensorTemperaturePot TempPot(REV7_pot_low, REV7_pot_high);
 #endif
 
 #ifdef ENABLE_AMBLIGHT_SENSOR
-// Normal 2 bit shift between raw and externally-presented values.
-static const uint8_t shiftRawScaleTo8Bit = 2;
-#ifdef ENABLE_AMBIENT_LIGHT_SENSOR_PHOTOTRANS_TEPT4400
-// This implementation expects a phototransitor TEPT4400 (50nA dark current, nominal 200uA@100lx@Vce=50V) from IO_POWER_UP to LDR_SENSOR_AIN and 220k to ground.
-// Measurement should be taken wrt to internal fixed 1.1V bandgap reference, since light indication is current flow across a fixed resistor.
-// Aiming for maximum reading at or above 100--300lx, ie decent domestic internal lighting.
-// Note that phototransistor is likely far more directionally-sensitive than LDR and its response nearly linear.
-// This extends the dynamic range and switches to measurement vs supply when full-scale against bandgap ref, then scales by Vss/Vbandgap and compresses to fit.
-// http://home.wlv.ac.uk/~in6840/Lightinglevels.htm
-// http://www.engineeringtoolbox.com/light-level-rooms-d_708.html
-// http://www.pocklington-trust.org.uk/Resources/Thomas%20Pocklington/Documents/PDF/Research%20Publications/GPG5.pdf
-// http://www.vishay.com/docs/84154/appnotesensors.pdf
-#if (7 == V0p2_REV) // REV7 initial board run especially uses different phototransistor (not TEPT4400).
-// Note that some REV7s from initial batch were fitted with wrong device entirely,
-// an IR device, with very low effective sensitivity (FSD ~ 20 rather than 1023).
-static const int LDR_THR_LOW = 180U;
-static const int LDR_THR_HIGH = 250U;
-#else // REV4 default values.
-static const int LDR_THR_LOW = 270U;
-static const int LDR_THR_HIGH = 400U;
-#endif
-#else // LDR (!defined(ENABLE_AMBIENT_LIGHT_SENSOR_PHOTOTRANS_TEPT4400))
-// This implementation expects an LDR (1M dark resistance) from IO_POWER_UP to LDR_SENSOR_AIN and 100k to ground.
-// Measurement should be taken wrt to supply voltage, since light indication is a fraction of that.
-// Values below from PICAXE V0.09 impl approx multiplied by 4+ to allow for scale change.
-#ifdef ENABLE_AMBLIGHT_EXTRA_SENSITIVE // Define if LDR not exposed to much light, eg for REV2 cut4 sideways-pointing LDR (TODO-209).
-static const int LDR_THR_LOW = 50U;
-static const int LDR_THR_HIGH = 70U;
-#else // Normal settings.
-static const int LDR_THR_LOW = 160U; // Was 30.
-static const int LDR_THR_HIGH = 200U; // Was 35.
-#endif // ENABLE_AMBLIGHT_EXTRA_SENSITIVE
-#endif // ENABLE_AMBIENT_LIGHT_SENSOR_PHOTOTRANS_TEPT4400
-AmbientLight AmbLight(LDR_THR_HIGH >> shiftRawScaleTo8Bit);
+AmbientLight AmbLight;
 #endif // ENABLE_AMBLIGHT_SENSOR
 
 #if defined(ENABLE_MINIMAL_ONEWIRE_SUPPORT)
