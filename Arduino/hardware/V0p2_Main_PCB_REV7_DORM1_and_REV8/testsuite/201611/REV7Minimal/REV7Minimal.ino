90da4e4f7997291414d017b710a083e3e51f58ef
denzo <denzo@denzo-desktop>
2016-11-09 12:00:35 +0000

    preparing ino skeleton

---
+++ Arduino/hardware/V0p2_Main_PCB_REV7_DORM1_and_REV8/testsuite/201611/REV7Minimal/REV7Minimal.ino
@@ -0,0 +1,53 @@
+/*
+The OpenTRV project licenses this file to you
+under the Apache Licence, Version 2.0 (the "Licence");
+you may not use this file except in compliance
+with the Licence. You may obtain a copy of the Licence at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licence is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the Licence for the
+specific language governing permissions and limitations
+under the Licence.
+
+Author(s) / Copyright (s): Deniz Erbilgin 2016
+*/
+/**
+ * Minimal REV7 config for testing power consumption.
+ * Aim is to:
+ *     - todo init GPIO pins to safe mode.
+ *     - todo init peripherals to safe low power mode.
+ *     - todo loop endlessly.
+ */
+
+// INCLUDES & DEFINES
+
+
+
+// OBJECTS & VARIABLES
+
+
+
+// FUNCTIONS
+/**
+ * @brief   Initialise GPIO pins and peripherals to a safe and low power mode.
+ */
+void setup()
+{
+
+}
+
+
+/**
+ * @brief   Loop endlessly doing nothing.
+ */
+void loop() { }
+
+
+
+/**
+ * @note    Power consumption figures.
+ */
