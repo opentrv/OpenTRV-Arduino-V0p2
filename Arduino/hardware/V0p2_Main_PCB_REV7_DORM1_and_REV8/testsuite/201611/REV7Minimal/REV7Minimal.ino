551dd51dcbca4de7c1533053f7b606d7615a7d01
denzo <denzo@denzo-desktop>
2016-11-09 13:45:47 +0000

    Compiles with RFM23B instance

--- Arduino/hardware/V0p2_Main_PCB_REV7_DORM1_and_REV8/testsuite/201611/REV7Minimal/REV7Minimal.ino
+++ Arduino/hardware/V0p2_Main_PCB_REV7_DORM1_and_REV8/testsuite/201611/REV7Minimal/REV7Minimal.ino
@@ -20,18 +20,65 @@ Author(s) / Copyright (s): Deniz Erbilgin 2016
  * Aim is to:
  *     - todo init GPIO pins to safe mode.
  *     - todo init peripherals to safe low power mode.
- *     - todo loop endlessly.
+ *     - loop endlessly.
  */
 
 // INCLUDES & DEFINES
-
+// REV7 / DORM1 all-in-one valve unit, secure TX.
+#define CONFIG_DORM1
+// Get defaults for valve applications.
+#include <OTV0p2_valve_ENABLE_defaults.h>
+// All-in-one valve unit (DORM1).
+#include <OTV0p2_CONFIG_REV7.h>
+// OTV0p2Base Libraries
+#include <OTV0p2Base.h>
+// Radio Libraries
+//#include <OTRadioLink.h>
+#include <OTRFM23BLink.h>
 
 
 // OBJECTS & VARIABLES
+/**
+ * Peripherals present on REV7
+ * - Pot        Set input to Hi-Z?
+ * - PHT        IO_POWER_UP LOW
+ * - Encoder    IO_POWER_UP LOW
+ * - LED        Set pin HIGH?
+ * - Button     Set to INPUT
+ * - H-Bridge   Control pins HIGH, Current sense set to input.
+ * - SHT21      read() once.
+ * - RFM23B     ??
+ * - XTAL       Setup and leave running?
+ * - UART       Disable
+ */
+
+/*
+ * Radio instance
+ */
+static constexpr bool RFM23B_allowRX = false;
+ 
+static constexpr uint8_t RFM23B_RX_QUEUE_SIZE = OTRFM23BLink::DEFAULT_RFM23B_RX_QUEUE_CAPACITY;
+static constexpr int8_t RFM23B_IRQ_PIN = -1;// PIN_RFM_NIRQ;
+// XXX Is it really worth having a separate primary radio in this case?
+OTRFM23BLink::OTRFM23BLink<OTV0P2BASE::V0p2_PIN_SPI_nSS, RFM23B_IRQ_PIN, RFM23B_RX_QUEUE_SIZE, RFM23B_allowRX> PrimaryRadio;//RFM23B;
+///* constexpr */ OTRadioLink::OTRadioLink &PrimaryRadio = RFM23B
+
+/*
+ * SHT21 instance
+ */
 
 
 
 // FUNCTIONS
+/**
+ * @brief   Set pins and on-board peripherals to safe low power state.
+ */
+
+/**
+ * @brief   Run POST.
+ */
+
+
 /**
  * @brief   Initialise GPIO pins and peripherals to a safe and low power mode.
  */
