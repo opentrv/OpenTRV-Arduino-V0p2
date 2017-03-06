53d03672b3a8270a6fe12ff01382b8f3b6e4073c
DamonHD <d@hd.org>
2017-03-06 02:53:04 +0000

    TODO-995: modifying tests for minimal hardware configs

--- v0p2_compilation_test.sh
+++ v0p2_compilation_test.sh
@@ -21,17 +21,32 @@
 BUILD_TARGET=opentrv:avr:opentrv_v0p2  # Target board to build for.
 SKETCH_PATH=$PWD/Arduino/hardware  # Path to hardware directory
 
+# List of minimal hardware configurations to be tested.
+# Partial paths to the individual .ino files under $SKETCH_PATH.
+PARTPATHS="
+    V0p2_Main_PCB_REV7_DORM1_and_REV8/REV7HardwareTest/REV7HardwareTest.ino
+    V0p2_Main_PCB_REV7_DORM1_and_REV8/REV8HardwareTest/REV8HardwareTest.ino
+    REV10/REV10HardwareTest/REV10HardwareTest.ino
+    REV11/REV11HardwareTest/REV11HardwareTest.ino
+    "
+
+# Loop through and test each configuration.
+# Note which one is being tested to make clear which one has failed, if any.
+for pp in $PARTPATHS;
+do
+    echo Testing $SKETCH_PATH/$pp
+    arduino --verify --board $BUILD_TARGET $SKETCH_PATH/$pp
+done
 
 # Tests
 # Script uses sh -e which will cause silent fail if test is not enclosed within the brackets below.
-
-(  # Put tests inside these brackets!
+#(  # Put tests inside these brackets!
 # Verify REV7 minimal test.
-arduino --verify --board $BUILD_TARGET $SKETCH_PATH/V0p2_Main_PCB_REV7_DORM1_and_REV8/REV7HardwareTest/REV7HardwareTest.ino
+#arduino --verify --board $BUILD_TARGET $SKETCH_PATH/V0p2_Main_PCB_REV7_DORM1_and_REV8/REV7HardwareTest/REV7HardwareTest.ino
 # Verify REV8 minimal test.
-arduino --verify --board $BUILD_TARGET $SKETCH_PATH/V0p2_Main_PCB_REV7_DORM1_and_REV8/REV8HardwareTest/REV8HardwareTest.ino
+#arduino --verify --board $BUILD_TARGET $SKETCH_PATH/V0p2_Main_PCB_REV7_DORM1_and_REV8/REV8HardwareTest/REV8HardwareTest.ino
 # Verify REV10 minimal test.
-arduino --verify --board $BUILD_TARGET $SKETCH_PATH/REV10/REV10HardwareTest/REV10HardwareTest.ino
+#arduino --verify --board $BUILD_TARGET $SKETCH_PATH/REV10/REV10HardwareTest/REV10HardwareTest.ino
 # Verify REV11 minimal test.
-arduino --verify --board $BUILD_TARGET $SKETCH_PATH/REV11/REV11HardwareTest/REV11HardwareTest.ino
-)
+#arduino --verify --board $BUILD_TARGET $SKETCH_PATH/REV11/REV11HardwareTest/REV11HardwareTest.ino
+#)
