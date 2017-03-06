08d9450e193843969bb307f6d5a1a657f6faf0fd
DamonHD <d@hd.org>
2017-03-06 03:07:44 +0000

    TODO-995: Adding hooks to compile all primary configs

---
+++ V0p2_test_primary_CONFIGs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+#
+# Test all primary configurations of V0p2_Main to be tested during CI.
+#
+# Copies V0p2_Main to a temporary working area
+# and adjusts its generic config header to test all primary configs in turn.
+
+# Name of generic config header which should #define one CONFIG_...
+GENERICCONFIGHEADER=V0p2_Generic_Config.h
+
+# Path to V0p2_Main sketch.
+MAIN=$PWD/Arduino/V0p2_Main
+
+echo Hello world
+exit 0
