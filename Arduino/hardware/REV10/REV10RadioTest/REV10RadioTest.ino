da169c7b07c1e8975647b8b34c2c817a8e4f7799
Denzo77 <denzo77@users.noreply.github.com>
2017-03-21 12:00:31 +0000

    OOK radio test

--- Arduino/hardware/REV10/REV10RadioTest/REV10RadioTest.ino
+++ Arduino/hardware/REV10/REV10RadioTest/REV10RadioTest.ino
@@ -95,38 +95,23 @@ OTV0P2BASE::SupplyVoltageCentiVolts Supply_cV;
 /*
  * RFM23B instance
  */
-static constexpr bool RFM23B_allowRX = true;
+static constexpr bool RFM23B_allowRX = false;
  
 static constexpr uint8_t RFM23B_RX_QUEUE_SIZE = OTRFM23BLink::DEFAULT_RFM23B_RX_QUEUE_CAPACITY;
 static constexpr int8_t RFM23B_IRQ_PIN = PIN_RFM_NIRQ;
 OTRFM23BLink::OTRFM23BLink<OTV0P2BASE::V0p2_PIN_SPI_nSS, RFM23B_IRQ_PIN, RFM23B_RX_QUEUE_SIZE, RFM23B_allowRX> PrimaryRadio;
 // Pick an appropriate radio config for RFM23 (if it is the primary radio).
 // Nodes talking on fast GFSK channel 0.
-static const uint8_t nPrimaryRadioChannels = 1;
+static constexpr uint8_t nPrimaryRadioChannels = 1;
+//static const OTRadioLink::OTRadioChannelConfig RFM23BConfigs[nPrimaryRadioChannels] = {
+//    // GFSK channel 0 full config, RX/TX, not in itself secure.
+//    OTRadioLink::OTRadioChannelConfig(OTRFM23BLink::StandardRegSettingsGFSK57600, true)
+//};
+// Nodes talking (including to to FHT8V) on slow OOK.
 static const OTRadioLink::OTRadioChannelConfig RFM23BConfigs[nPrimaryRadioChannels] = {
-    // GFSK channel 0 full config, RX/TX, not in itself secure.
-    OTRadioLink::OTRadioChannelConfig(OTRFM23BLink::StandardRegSettingsGFSK57600, true), };
-
-
-/**
- * SIM900 instance
- */
-// SIM card configs
-static const char SIM900_PIN[5] PROGMEM       = "1111";
-static const char SIM900_APN[] PROGMEM      = "\"mobiledata\""; // GeoSIM
-// UDP Configs - Edit SIM900_UDP_ADDR for relevant server. NOTE: The server IP address should never be committed to GitHub.
-static const char SIM900_UDP_ADDR[16] PROGMEM = ""; // Of form "1.2.3.4".
-static const char SIM900_UDP_PORT[5] PROGMEM = "9999";             // Standard port for OpenTRV servers
-const OTSIM900Link::OTSIM900LinkConfig_t SIM900Config(
-                                                false,
-                                                SIM900_PIN,
-                                                SIM900_APN,
-                                                SIM900_UDP_ADDR,
-                                                SIM900_UDP_PORT);
-static const OTRadioLink::OTRadioChannelConfig SecondaryRadioConfig(&SIM900Config, true, true, true);
-OTSIM900Link::OTSIM900Link<8, 5, RADIO_POWER_PIN, OTV0P2BASE::getSecondsLT> SecondaryRadio; // (REGULATOR_POWERUP, RADIO_POWER_PIN);
-
-
+  // FS20/FHT8V compatible channel 0 partial/minimal single-channel register config; RX/TX, not secure, unframed.
+  OTRadioLink::OTRadioChannelConfig(OTRFM23BLink::FHT8V_RFM23_Reg_Values, false, true, true, false, false, true)
+};
 
 /*
  * TMP112 instance
@@ -210,15 +195,9 @@ void setup()
     // Check that the radio is correctly connected; panic if not...
     if(!PrimaryRadio.configure(nPrimaryRadioChannels, RFM23BConfigs) || !PrimaryRadio.begin()) { panic(F("r1")); }
 
-  // Turn power on for SIM900 with PFET for secondary power control.
-    fastDigitalWrite(A3, 0);
+  // Turn power off for SIM900 with PFET for secondary power control.
+    fastDigitalWrite(A3, HIGH);
     pinMode(A3, OUTPUT);
-  // Initialise the radio, if configured, ASAP because it can suck a lot of power until properly initialised.
-    SecondaryRadio.preinit(NULL);
-//  DEBUG_SERIAL_PRINTLN_FLASHSTRING("R2");
-  // Check that the radio is correctly connected; panic if not...
-    if(!SecondaryRadio.configure(1, &SecondaryRadioConfig) || !SecondaryRadio.begin()) { panic(F("r2")); }
-  // Assume no RX nor filtering on secondary radio.
 
     // Collect full set of environmental values before entering loop() in normal mode.
     // This should also help ensure that sensors are properly initialised.
@@ -243,8 +222,7 @@ void setup()
     OTV0P2BASE::LED_HEATCALL_OFF();
 
     // Do OpenTRV-specific (late) setup.
-    PrimaryRadio.listen(true);
-    SecondaryRadio.listen(false);
+    PrimaryRadio.listen(false);
 }
 
 
@@ -272,24 +250,16 @@ void loop()
     DEBUG_SERIAL_PRINT(light);
     DEBUG_SERIAL_PRINTLN();
 
-    // Very crude poll/RX; likely to miss plenty.
-    PrimaryRadio.poll();
-    const uint8_t rxed = PrimaryRadio.getRXMsgsQueued();
-    DEBUG_SERIAL_PRINT_FLASHSTRING("RXED: ");
-    DEBUG_SERIAL_PRINT(rxed);
-    DEBUG_SERIAL_PRINTLN();
-    if(0 != rxed)
-      {
-      DEBUG_SERIAL_PRINT_FLASHSTRING("RX 1st byte: ");
-      DEBUG_SERIAL_PRINTFMT(*PrimaryRadio.peekRXMsg(), HEX);
+    // Very crude Tx..
+    static const uint8_t txBuffer[] = "aaaaaaaa{1234567890}";
+    if(!PrimaryRadio.queueToSend(txBuffer, sizeof(txBuffer), 0, OTRadioLink::OTRadioLink::TXnormal)) {
+      DEBUG_SERIAL_PRINT("!Tx");
       DEBUG_SERIAL_PRINTLN();
-      PrimaryRadio.removeRXMsg();
-      }
-    SecondaryRadio.poll();
+    }
+    PrimaryRadio.poll();
 
     // =====
     // To sleep, perchance to dream...
-
     // Ensure that serial I/O is off while sleeping.
     OTV0P2BASE::powerDownSerial();
     // Power down most stuff (except radio for hub RX).
@@ -297,7 +267,6 @@ void loop()
     // Normal long minimal-power sleep until wake-up interrupt.
     // Rely on interrupt to force quick loop round to I/O poll.
     OTV0P2BASE::sleepUntilInt();
-
     LED_on = !LED_on;
 }
 
