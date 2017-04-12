44691eb71f26e792f83ea870588684d941f17a82
Denzo77 <denzo77@users.noreply.github.com>
2017-04-12 15:27:01 +0100

    Working with one ino file & header containing server ip

--- Arduino/hardware/REV10/REV10MessagePathTest/REV10MinSecureRelay/REV10MinSecureRelay.ino
+++ Arduino/hardware/REV10/REV10MessagePathTest/REV10MinSecureRelay/REV10MinSecureRelay.ino
@@ -30,39 +30,76 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2017
   Basic AVR power consumption ticking an (empty) control loop at ~0.5Hz should be ~1uA.
  */
 
-#include "REV10SecureBHR.h"
 
+// GLOBAL flags that alter system build and behaviour.
+//#define DEBUG // If defined, do extra checks and serial logging.  Will take more code space and power.
 
-// Indicate that the system is broken in an obvious way (distress flashing the main LED).
-// DOES NOT RETURN.
-// Tries to turn off most stuff safely that will benefit from doing so, but nothing too complex.
-// Tries not to use lots of energy so as to keep distress beacon running for a while.
-void panic()
-  {
-  // Reset radio and go into low-power mode.
-  PrimaryRadio.panicShutdown();
-  // Reset radio and go into low-power mode.
-  SecondaryRadio.panicShutdown();
-  // Power down almost everything else...
-  OTV0P2BASE::minimisePowerWithoutSleep();
-  pinMode(OTV0P2BASE::LED_HEATCALL_L, OUTPUT);
-  for( ; ; )
-    {
-    OTV0P2BASE::LED_HEATCALL_ON();
-    OTV0P2BASE::nap(WDTO_15MS);
-    OTV0P2BASE::LED_HEATCALL_OFF();
-    OTV0P2BASE::nap(WDTO_120MS);
-    }
-  }
+// Ensure that OpenTRV 'standard' UART speed is set unless explicitly overridden.
+#define BAUD 4800
+// Global flag for REV10 secure BHR
+#define CONFIG_REV10_SECURE_BHR // REV10: secure stats relay and boiler hub.
 
-// Panic with fixed message.
-void panic(const __FlashStringHelper *s)
-  {
-  OTV0P2BASE::serialPrintlnAndFlush(); // Start new line to highlight error.  // May fail.
-  OTV0P2BASE::serialPrintAndFlush('!'); // Indicate error with leading '!' // May fail.
-  OTV0P2BASE::serialPrintlnAndFlush(s); // Print supplied detail text. // May fail.
-  panic();
-  }
+// Get defaults for valve applications.
+#include <OTV0p2_valve_ENABLE_defaults.h>
+// REV8 + GSM Arduino shield + I2CEXT, see TODO-551.
+#include <OTV0p2_CONFIG_REV10.h>
+// --------------------------------------------
+// Fixups to apply after loading the target config.
+#include <OTV0p2_valve_ENABLE_fixups.h>
+
+#include <OTV0p2_Board_IO_Config.h> // I/O pin allocation and setup: include ahead of I/O module headers.
+
+#include <Arduino.h>
+#include <OTV0p2Base.h>
+#include <OTRadioLink.h>
+#include <OTRFM23BLink.h>
+#include <OTSIM900Link.h>
+#include <OTAESGCM.h>
+
+#include "ipAddress.h"  // IP adress in seperate header to avoid accidentally committing.
+
+/////// SENSORS
+// Sensor for supply (eg battery) voltage in millivolts.
+OTV0P2BASE::SupplyVoltageCentiVolts Supply_cV;
+OTV0P2BASE::RoomTemperatureC16_TMP112 TemperatureC16;
+
+/////// / RADIOS
+//For EEPROM: TODO make a spec for how config should be stored in EEPROM to make changing them easy
+//- Set the first field of SIM900LinkConfig to true.
+//- The configs are stored as \0 terminated strings starting at 0x300.
+//- You can program the eeprom using ./OTRadioLink/dev/utils/sim900eepromWrite.ino
+//  static const void *SIM900_PIN      = (void *)0x0300;
+//  static const void *SIM900_APN      = (void *)0x0305;
+//  static const void *SIM900_UDP_ADDR = (void *)0x031B;
+//  static const void *SIM900_UDP_PORT = (void *)0x0329;
+//  const OTSIM900Link::OTSIM900LinkConfig_t SIM900Config(
+//                                                  true,
+//                                                  SIM900_PIN,
+//                                                  SIM900_APN,
+//                                                  SIM900_UDP_ADDR,
+//                                                  SIM900_UDP_PORT);
+//For Flash:
+//- Set the first field of SIM900LinkConfig to false.
+//- The configs are stored as \0 terminated strings.
+//- Where multiple options are available, uncomment whichever you want
+  static const char SIM900_PIN[5] PROGMEM       = "1111";
+
+// APN Configs - Uncomment based on what SIM you are using
+//  static const char SIM900_APN[] PROGMEM      = "\"everywhere\",\"eesecure\",\"secure\""; // EE
+//static const char SIM900_APN[] PROGMEM      = "\"arkessa.net\",\"arkessa\",\"arkessa\""; // Arkessa
+static const char SIM900_APN[] PROGMEM      = "\"mobiledata\""; // GeoSIM
+
+// UDP Configs - Edit SIM900_UDP_ADDR for relevant server. NOTE: The server IP address should never be committed to GitHub.
+// IP adress in seperate header to avoid accidentally committing.
+//static const char SIM900_UDP_ADDR[16] PROGMEM = ""; // Of form "1.2.3.4". 
+static const char SIM900_UDP_PORT[5] PROGMEM = "9999";             // Standard port for OpenTRV servers
+const OTSIM900Link::OTSIM900LinkConfig_t SIM900Config(
+                                                false,
+                                                SIM900_PIN,
+                                                SIM900_APN,
+                                                SIM900_UDP_ADDR,
+                                                SIM900_UDP_PORT);
+
 
 // Pick an appropriate radio config for RFM23 (if it is the primary radio).
 // Nodes talking on fast GFSK channel 0.
@@ -72,16 +109,20 @@ static const OTRadioLink::OTRadioChannelConfig RFM23BConfigs[nPrimaryRadioChanne
   // GFSK channel 0 full config, RX/TX, not in itself secure.
   OTRadioLink::OTRadioChannelConfig(OTRFM23BLink::StandardRegSettingsGFSK57600, true),
   };
-
 static const OTRadioLink::OTRadioChannelConfig SecondaryRadioConfig(&SIM900Config, true);
 
-/////// SENSORS
+// Brings in necessary radio libs.
+static constexpr uint8_t RFM23B_RX_QUEUE_SIZE = OTRFM23BLink::DEFAULT_RFM23B_RX_QUEUE_CAPACITY;
+static constexpr int8_t RFM23B_IRQ_PIN = PIN_RFM_NIRQ;
+static constexpr bool RFM23B_allowRX = true;
+OTRFM23BLink::OTRFM23BLink<OTV0P2BASE::V0p2_PIN_SPI_nSS, RFM23B_IRQ_PIN, RFM23B_RX_QUEUE_SIZE, RFM23B_allowRX> RFM23B;
+OTSIM900Link::OTSIM900Link<8, 5, RADIO_POWER_PIN, OTV0P2BASE::getSecondsLT> SIM900;
 
-// Sensor for supply (eg battery) voltage in millivolts.
-OTV0P2BASE::SupplyVoltageCentiVolts Supply_cV;
+// Assigns radio to PrimaryRadio alias
+OTRadioLink::OTRadioLink &PrimaryRadio = RFM23B;
 
-OTV0P2BASE::RoomTemperatureC16_TMP112 TemperatureC16;
-
+// Assign radio to SecondaryRadio alias.
+OTRadioLink::OTRadioLink &SecondaryRadio = SIM900;
 
 //////// Constants and variables
 // Controller's view of Least Significant Digits of the current (local) time, in this case whole seconds.
@@ -98,6 +139,197 @@ static uint8_t minuteCount;
 static constexpr uint8_t PP_OFF_MS = 250;
 // Mask for Port B input change interrupts.
 static constexpr uint8_t RFM23B_INT_MASK = (1 << (PIN_RFM_NIRQ&7));
+
+
+//========================================
+// LOCAL FUNCTIONS
+//========================================
+// Indicate that the system is broken in an obvious way (distress flashing the main LED).
+// DOES NOT RETURN.
+// Tries to turn off most stuff safely that will benefit from doing so, but nothing too complex.
+// Tries not to use lots of energy so as to keep distress beacon running for a while.
+static void panic()
+  {
+  // Reset radio and go into low-power mode.
+  PrimaryRadio.panicShutdown();
+  // Reset radio and go into low-power mode.
+  SecondaryRadio.panicShutdown();
+  // Power down almost everything else...
+  OTV0P2BASE::minimisePowerWithoutSleep();
+  pinMode(OTV0P2BASE::LED_HEATCALL_L, OUTPUT);
+  for( ; ; )
+    {
+    OTV0P2BASE::LED_HEATCALL_ON();
+    OTV0P2BASE::nap(WDTO_15MS);
+    OTV0P2BASE::LED_HEATCALL_OFF();
+    OTV0P2BASE::nap(WDTO_120MS);
+    }
+  }
+// Panic with fixed message.
+static void panic(const __FlashStringHelper *s)
+  {
+  OTV0P2BASE::serialPrintlnAndFlush(); // Start new line to highlight error.  // May fail.
+  OTV0P2BASE::serialPrintAndFlush('!'); // Indicate error with leading '!' // May fail.
+  OTV0P2BASE::serialPrintlnAndFlush(s); // Print supplied detail text. // May fail.
+  panic();
+  }
+
+// Returns true on successful frame type match, false if no suitable frame was found/decoded and another parser should be tried.
+static void decodeAndHandleOTSecureableFrame(Print *p, const uint8_t * const msg)
+  {
+  const uint8_t msglen = msg[-1];
+  if(msglen < 2) { return; } // Too short to be useful, so ignore.
+  const uint8_t firstByte = msg[0];
+
+  // Validate structure of header/frame first.
+  // This is quick and checks for insane/dangerous values throughout.
+  OTRadioLink::SecurableFrameHeader sfh;
+  const uint8_t l = sfh.checkAndDecodeSmallFrameHeader(msg-1, msglen+1);
+  // If isOK flag is set false for any reason, frame is broken/unsafe/unauth.
+  bool isOK = (l > 0);
+  // If failed this early and this badly, let someone else try parsing the message buffer...
+  if(!isOK) { return; }
+
+  // Buffer for receiving secure frame body.
+  // (Non-secure frame bodies should be read directly from the frame buffer.)
+  uint8_t secBodyBuf[OTRadioLink::ENC_BODY_SMALL_FIXED_PTEXT_MAX_SIZE];
+  uint8_t decryptedBodyOutSize = 0;
+
+  // Validate integrity of frame (CRC for non-secure, auth for secure).
+  const bool secureFrame = sfh.isSecure();
+  // Body length after any decryption, etc.
+  uint8_t receivedBodyLength;
+  // TODO: validate entire message, eg including auth, or CRC if insecure msg rcvd&allowed.
+  // Validate (authenticate) and decrypt body of secure frames.
+  uint8_t key[16];
+  if(secureFrame && isOK)
+    {
+    // Get the 'building' key.
+    if(!OTV0P2BASE::getPrimaryBuilding16ByteSecretKey(key))
+      {
+      isOK = false;
+      OTV0P2BASE::serialPrintlnAndFlush(F("!RX key"));
+      }
+    }
+  uint8_t senderNodeID[OTV0P2BASE::OpenTRV_Node_ID_Bytes];
+  if(secureFrame && isOK)
+    {
+    // Look up full ID in associations table,
+    // validate RX message counter,
+    // authenticate and decrypt,
+    // update RX message counter.
+    isOK = (0 != OTRadioLink::SimpleSecureFrame32or0BodyRXV0p2::getInstance().decodeSecureSmallFrameSafely(&sfh, msg-1, msglen+1,
+                                            OTAESGCM::fixed32BTextSize12BNonce16BTagSimpleDec_DEFAULT_STATELESS,
+                                            NULL, key,
+                                            secBodyBuf, sizeof(secBodyBuf), decryptedBodyOutSize,
+                                            senderNodeID,
+                                            true));
+    if(!isOK)
+      {
+      // Useful brief network diagnostics: a couple of bytes of the claimed ID of rejected frames.
+      // Warnings rather than errors because there may legitimately be multiple disjoint networks.
+      OTV0P2BASE::serialPrintAndFlush(F("?RX auth")); // Missing association or failed auth.
+      if(sfh.getIl() > 0) { OTV0P2BASE::serialPrintAndFlush(' '); OTV0P2BASE::serialPrintAndFlush(sfh.id[0], HEX); }
+      if(sfh.getIl() > 1) { OTV0P2BASE::serialPrintAndFlush(' '); OTV0P2BASE::serialPrintAndFlush(sfh.id[1], HEX); }
+      OTV0P2BASE::serialPrintlnAndFlush();
+      }
+    }
+
+  if(!isOK) { return; } // Stop if not OK.
+
+  switch(firstByte) // Switch on type. XXX
+    {
+    case 'O' | 0x80: // Basic OpenTRV secure frame...
+      {
+      if(decryptedBodyOutSize < 2)
+        { break; }
+      // If the frame contains JSON stats
+      // then forward entire secure frame as-is across the secondary radio relay link,
+      // else print directly to console/Serial.
+      if((0 != (secBodyBuf[1] & 0x10)) && (decryptedBodyOutSize > 3) && ('{' == secBodyBuf[2]))
+        { SecondaryRadio.queueToSend(msg, msglen); }
+      return;
+      }
+    // Reject unrecognised type, though fall through potentially to recognise other encodings.
+    default: break;
+    }
+
+  // Failed to parse; let another handler try.
+  return;
+  }
+
+// Incrementally process I/O and queued messages, including from the radio link.
+// This may mean printing them to Serial (which the passed Print object usually is),
+// or adjusting system parameters,
+// or relaying them elsewhere, for example.
+// This will write any output to the supplied Print object,
+// typically the Serial output (which must be running if so).
+// This will attempt to process messages in such a way
+// as to avoid internal overflows or other resource exhaustion,
+// which may mean deferring work at certain times
+// such as the end of minor cycle.
+// The Print object pointer must not be NULL.
+bool handleQueuedMessages(Print *p, bool wakeSerialIfNeeded, OTRadioLink::OTRadioLink *rl)
+  {
+  // Avoid starting any potentially-slow processing very late in the minor cycle.
+  // This is to reduce the risk of loop overruns
+  // at the risk of delaying some processing
+  // or even dropping some incoming messages if queues fill up.
+  // Decoding (and printing to serial) a secure 'O' frame takes ~60 ticks (~0.47s).
+  // Allow for up to 0.5s of such processing worst-case,
+  // ie don't start processing anything later that 0.5s before the minor cycle end.
+  const uint8_t sctStart = OTV0P2BASE::getSubCycleTime();
+  if(sctStart >= ((OTV0P2BASE::GSCT_MAX/4)*3)) { return(false); }
+
+  // Deal with any I/O that is queued.
+    // Poll for inbound frames.
+    // If RX is not interrupt-driven then
+    // there will usually be little time to do this
+    // before getting an RX overrun or dropped frame.
+    PrimaryRadio.poll();
+    SecondaryRadio.poll();
+
+  // XXX can probably strip these out.
+  bool workDone = false;
+  bool neededWaking = false; // Set true once this routine wakes Serial.
+  const volatile uint8_t *pb;
+  if(NULL != (pb = rl->peekRXMsg()))
+    {
+    if(wakeSerialIfNeeded && OTV0P2BASE::powerUpSerialIfDisabled<V0P2_UART_BAUD>()) { neededWaking = true; } // FIXME
+    // Don't currently regard anything arriving over the air as 'secure'.
+    // FIXME: shouldn't have to cast away volatile to process the message content.
+    decodeAndHandleOTSecureableFrame(p, (const uint8_t *)pb);
+    rl->removeRXMsg();
+    // Note that some work has been done.
+    workDone = true;
+    }
+
+  // Turn off serial at end, if this routine woke it.
+  if(neededWaking) { OTV0P2BASE::flushSerialProductive(); OTV0P2BASE::powerDownSerial(); } // XXX move into if clause
+  return(workDone);
+  }
+
+//========================================
+// INTERRUPT SERVICE ROUTINES
+//========================================
+  //// Interrupt count.  Marked volatile so safe to read without a lock as is a single byte.
+//static volatile uint8_t intCountPB;
+// Previous state of port B pins to help detect changes.
+static volatile uint8_t prevStatePB;
+// Interrupt service routine for PB I/O port transition changes.
+ISR(PCINT0_vect)
+  {
+//  ++intCountPB;
+  const uint8_t pins = PINB;
+  const uint8_t changes = pins ^ prevStatePB;
+  prevStatePB = pins;
+  // RFM23B nIRQ falling edge is of interest.
+  // Handler routine not required/expected to 'clear' this interrupt.
+  // TODO: try to ensure that OTRFM23BLink.handleInterruptSimple() is inlineable to minimise ISR prologue/epilogue time and space.
+  if((changes & RFM23B_INT_MASK) && !(pins & RFM23B_INT_MASK))
+    { PrimaryRadio.handleInterruptSimple(); }
+  }
+
 
 //========================================
 // SETUP
@@ -293,24 +525,5 @@ void loop()
   // End-of-loop processing, that may be slow.
   // Ensure progress on queued messages ahead of slow work.  (TODO-867)
   handleQueuedMessages(&Serial, true, &PrimaryRadio); // Deal with any pending I/O.
-  }
-
-
-  //// Interrupt count.  Marked volatile so safe to read without a lock as is a single byte.
-//static volatile uint8_t intCountPB;
-// Previous state of port B pins to help detect changes.
-static volatile uint8_t prevStatePB;
-// Interrupt service routine for PB I/O port transition changes.
-ISR(PCINT0_vect)
-  {
-//  ++intCountPB;
-  const uint8_t pins = PINB;
-  const uint8_t changes = pins ^ prevStatePB;
-  prevStatePB = pins;
-  // RFM23B nIRQ falling edge is of interest.
-  // Handler routine not required/expected to 'clear' this interrupt.
-  // TODO: try to ensure that OTRFM23BLink.handleInterruptSimple() is inlineable to minimise ISR prologue/epilogue time and space.
-  if((changes & RFM23B_INT_MASK) && !(pins & RFM23B_INT_MASK))
-    { PrimaryRadio.handleInterruptSimple(); }
   }
 
