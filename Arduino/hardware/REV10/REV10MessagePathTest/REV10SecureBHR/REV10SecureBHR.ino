8ac2c61b4a7273faaf0fa56491d3f4a9ee7d37c1
Denzo77 <denzo77@users.noreply.github.com>
2017-07-20 17:12:02 +0100

    comments

--- Arduino/hardware/REV10/REV10MessagePathTest/REV10SecureBHR/REV10SecureBHR.ino
+++ Arduino/hardware/REV10/REV10MessagePathTest/REV10SecureBHR/REV10SecureBHR.ino
@@ -57,45 +57,9 @@ Author(s) / Copyright (s): Damon Hart-Davis 2013--2017
 
 #include "secondaryRadioConfig.h"
 
-//#include "ipAddress.h"  // IP adress in seperate header to avoid accidentally committing.
+//#include "secondaryRadioConfig.h"  // IP address in separate header to avoid accidentally committing.
 
 /////// RADIOS
-////For EEPROM: TODO make a spec for how config should be stored in EEPROM to make changing them easy
-////- Set the first field of SIM900LinkConfig to true.
-////- The configs are stored as \0 terminated strings starting at 0x300.
-////- You can program the eeprom using ./OTRadioLink/dev/utils/sim900eepromWrite.ino
-////  static const void *SIM900_PIN      = (void *)0x0300;
-////  static const void *SIM900_APN      = (void *)0x0305;
-////  static const void *SIM900_UDP_ADDR = (void *)0x031B;
-////  static const void *SIM900_UDP_PORT = (void *)0x0329;
-////  const OTSIM900Link::OTSIM900LinkConfig_t SIM900Config(
-////                                                  true,
-////                                                  SIM900_PIN,
-////                                                  SIM900_APN,
-////                                                  SIM900_UDP_ADDR,
-////                                                  SIM900_UDP_PORT);
-////For Flash:
-////- Set the first field of SIM900LinkConfig to false.
-////- The configs are stored as \0 terminated strings.
-////- Where multiple options are available, uncomment whichever you want
-//  static const char SIM900_PIN[5] PROGMEM       = "1111";
-//
-//// APN Configs - Uncomment based on what SIM you are using
-////  static const char SIM900_APN[] PROGMEM      = "\"everywhere\",\"eesecure\",\"secure\""; // EE
-////static const char SIM900_APN[] PROGMEM      = "\"arkessa.net\",\"arkessa\",\"arkessa\""; // Arkessa
-//static const char SIM900_APN[] PROGMEM      = "\"mobiledata\""; // GeoSIM
-//
-//// UDP Configs - Edit SIM900_UDP_ADDR for relevant server. NOTE: The server IP address should never be committed to GitHub.
-//// IP adress in seperate header to avoid accidentally committing.
-//static const char SIM900_UDP_ADDR[16] PROGMEM = ""; // Of form "1.2.3.4".
-//static const char SIM900_UDP_PORT[5] PROGMEM = "9999";             // Standard port for OpenTRV servers
-//const OTSIM900Link::OTSIM900LinkConfig_t SIM900Config(
-//                                                false,
-//                                                SIM900_PIN,
-//                                                SIM900_APN,
-//                                                SIM900_UDP_ADDR,
-//                                                SIM900_UDP_PORT);
-
 // Pick an appropriate radio config for RFM23 (if it is the primary radio).
 // Nodes talking on fast GFSK channel 0.
 static constexpr uint8_t nPrimaryRadioChannels = 1;
